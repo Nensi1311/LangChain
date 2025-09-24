404bc3be7d1ee6b448414877873542837a1e02d9
Nensi Pansuriya <nensipansuriya1311@gmail.com>
2025-09-24 11:43:38 +0530

    typeddict with structured output

---
+++ Structured-Output/with_structured_output_typeddict.py
@@ -0,0 +1,20 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from typing import TypedDict
+
+load_dotenv()
+
+model = ChatOpenAI()
+
+class Review(TypedDict):
+    summary: str
+    sentiment: str
+
+structured_model = model.with_structured_output(Review)
+
+result = model.invoke("""The Hardware is great, but the software feels bloated. There are too many pre-installed apps that I can't remove. Also, the UI looks outdated compared to other brands. Hoping for a software update to fix this.""")
+
+print(result)
+print(type(result))
+print(result['summary'])
+print(result['sentiment'])
