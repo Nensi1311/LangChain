ab2efa96cd0f9f5db54b95c43d011a2f4a5e1b3c
Nensi Pansuriya <nensipansuriya1311@gmail.com>
2025-09-25 11:31:34 +0530

    json output parser

---
+++ Output-Parser/JsonOutputParser.py
@@ -0,0 +1,39 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+
+load_dotenv()
+
+# Define the model
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser = JsonOutputParser()
+
+template = PromptTemplate(
+    template='Give me 5 facts about {topic} \n {format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+# Method-1
+prompt = template.format()
+print(prompt)
+
+
+result = model.invoke(prompt)
+print(result)
+
+
+final_result = parser.parse(result.content)
+print(final_result)
+
+# Method-2
+chain = template | model | parser
+result2 = chain.invoke({'topic':'black hole'})
+print(result2)
